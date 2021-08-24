17a45823409ac36625c0dcfa0c1de8ddd9c035ac
Yi Wang <yiw17@student.unimelb.edu.au>
2021-08-24 14:06:36 +0800

    Add mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+/* Connect database */
+connectDB();
+
+/* Handle GET request */
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
