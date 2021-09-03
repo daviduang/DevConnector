8203cb81a6a23afb115e30c8d8bbfdcea0bc12b7
Yi Wang <yiw17@student.unimelb.edu.au>
2021-09-03 12:52:20 +0800

    Update posts.js
    + Like Post: PUT
    + Unlike Post: PUT

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -113,4 +113,62 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+/**
+ * @route    PUT api/posts/like/:id
+ * @desc     Like a post
+ * @access   Private
+*/
+router.put('/like/:id', auth, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // check if post is already by the user
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post already been liked' })
+        }
+
+        // add the userId to the 'likes' array in target 'post'
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+        res.json(post.likes);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send();
+    }
+});
+
+/**
+ * @route    PUT api/posts/unlike/:id
+ * @desc     Unlike a post
+ * @access   Private
+*/
+router.put('/unlike/:id', auth, async(req, res) => {
+    try {
+
+        // get the corresponding post by the request post id
+        const post = await Post.findById(req.params.id);
+
+        // check if post is already by the user
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length == 0) {
+            return res.status(400).json({ msg: 'Post has not been liked' })
+        }
+
+        // get the index of removed the user
+        // convert 'likes' array's user into string, then get the index of the request user in 'likes' array
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id)
+
+        // from remove index, remove  1 element
+        post.likes.splice(removeIndex, 1)
+
+        await post.save();
+        res.json(post.likes);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send();
+    }
+});
+
 module.exports = router;
