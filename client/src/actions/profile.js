b225fe95ffe42355b7f52e4a71f1364fcd8376de
Yi Wang <yiw17@student.unimelb.edu.au>
2021-09-16 15:06:55 +0800

    CreateProfile.js: submit profile form action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { setAlert } from "./alert";
 import { GET_PROFILE, PROFILE_ERROR } from "./types";
 
 // Get current user's profile
@@ -20,3 +21,46 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+// Create/Update Profile
+export const createProfile =
+  (formData, history, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const res = await axios.post("/api/profile", formData, config);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+
+      // Show if data is updated or created
+      dispatch(setAlert(edit ? "Profile Updated" : "Profile Created"));
+
+      if (!edit) {
+        history.push("/dashboard");
+      }
+    } catch (error) {
+      // Collect all error msgs sent from server
+      const errors = error.response.data.errors;
+
+      if (errors) {
+        // For each error, print an error alert
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: error.response.statusText,
+          status: error.response.status,
+        },
+      });
+    }
+  };
