643e03b2f9a96f33bd47ae531ee95c5e6fcff733
Yi Wang <yiw17@student.unimelb.edu.au>
2021-09-16 15:52:21 +0800

    fix "go back" link

--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -240,9 +240,9 @@ const EditProfile = ({
 
         <input type="submit" className="btn btn-primary my-1" />
 
-        <a href="dashboard.html" className="btn btn-light my-1">
+        <Link to="/dashboard" className="btn btn-light my-1">
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
